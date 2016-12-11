78a8c7b08a4d63c675bf9ec25dfce76227cc2571
oscarf <k3ther@gmail.com>
2016-12-12 00:21:33 +0100

    Added navbar in multiple activities

--- app/src/main/java/dam/easypocket/CollectionList.java
+++ app/src/main/java/dam/easypocket/CollectionList.java
@@ -3,21 +3,15 @@ package dam.easypocket;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class CollectionList extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class CollectionList extends BaseActivity {
 
     private String currentCollectionSelected;
     private String collectionName;
@@ -25,10 +19,12 @@ public class CollectionList extends AppCompatActivity
     private Button addElement;
     private Button editDesign;
     private Button explore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_test);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -43,8 +39,6 @@ public class CollectionList extends AppCompatActivity
         LinearLayout ll = (LinearLayout) findViewById(R.id.linSV);
 
         CollectionDBHelper db = new CollectionDBHelper(this.getApplicationContext());
-
-
 
         addElement = (Button)findViewById(R.id.buttonAddElement);
         addElement.setOnClickListener(new View.OnClickListener() {
@@ -108,63 +102,5 @@ public class CollectionList extends AppCompatActivity
                 });
             }
         }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_test, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_collections) {
-//            Intent intent = new Intent(this, CollectionList.class);
-//            startActivity(intent);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
--- app/src/main/java/dam/easypocket/EditCollection.java
+++ app/src/main/java/dam/easypocket/EditCollection.java
@@ -2,20 +2,22 @@ package dam.easypocket;
 
 import android.database.Cursor;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 //hace referencia a la 1.b
-public class EditCollection extends AppCompatActivity {
+public class EditCollection extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     private String currentCollection;
     private TextView currentNameCollection;
@@ -27,11 +29,22 @@ public class EditCollection extends AppCompatActivity {
 
     private Button addField;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_collection);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         currentCollection = getIntent().getExtras().getString("currentCollectionSelected");
 
         currentNameCollection = (TextView)findViewById(R.id.nombreColeccion);
