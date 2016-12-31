99d23e09a01a4034dc68d2ede1c4ea24fc953b3a
oscarf <k3ther@gmail.com>
2016-12-31 19:59:41 +0100

    Added nav bar to summary

--- app/src/main/java/dam/easypocket/SummaryVisual.java
+++ app/src/main/java/dam/easypocket/SummaryVisual.java
@@ -2,8 +2,11 @@ package dam.easypocket;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -11,7 +14,8 @@ import android.widget.TextView;
 
 
 //hace referencia a la 1.e
-public class SummaryVisual extends AppCompatActivity {
+public class SummaryVisual extends BaseActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private String currentCollection;
     private CollectionDBHelper db;
@@ -20,6 +24,17 @@ public class SummaryVisual extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary_visual);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         currentCollection = getIntent().getExtras().getString("currentCollectionSelected");
         db = new CollectionDBHelper(this.getApplicationContext());
